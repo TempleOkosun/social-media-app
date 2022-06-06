8e0da0ca9903313dc6939fb18877c4432368a20d
Temple Okosun <okosun.temple@gmail.com>
2022-06-06 17:55:51 -0400

    database setup completed

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 
 const morgan = require('morgan')
+const dotenv = require('dotenv')
+dotenv.config()
 
 // Import routes
 const postRoutes = require('./routes/post')
@@ -13,7 +15,18 @@ app.use(morgan("dev"))
 // App route
 app.use('/', postRoutes);
 
-const port = 8000
+
+// db
+const mongoose = require('mongoose')
+const mongoURL = process.env.MONGO_URI
+mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true })
+const connection = mongoose.connection
+connection.on('error', console.error.bind(console, 'connection error:'))
+connection.on('connected', function () {
+    console.log('MongoDB Connection Successful.')
+})
+
+const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log(`The API is listening on port: ${port}`)
 })
