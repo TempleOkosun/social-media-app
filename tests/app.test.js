c5e879c51dcc1fc0bc34cbd1706b69d0a4ffce0a
Temple <okosun.temple@gmail.com>
2022-06-09 21:17:58 -0400

    prepared test database connection with mongodb-memory-server

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
-const mongodb = require('./utils/db')
-const app = require('./app') // Link to the app file
+const mongodb = require('../tests/config/db')
+const app = require('../app') // Link to the app file
 
 const request = require('supertest')
 const cookieParser = require('cookie-parser')
@@ -7,7 +7,9 @@ const assert = require('assert')
 // const should = require('should')
 
 // db
-mongodb.connect(process.env.MONGO_URI)
+beforeAll(async () => await mongodb.connect())
+afterEach(async () => await mongodb.clear())
+afterAll(async () => await mongodb.close())
 
 describe('given an email, name, password and confirm password', () => {
   test('should respond with a 200 status code', async () => {
---
+++ tests/config/db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const { MongoMemoryServer } = require('mongodb-memory-server')
+
+let connection
+let mongodb
+
+const connect = async () => {
+  mongodb = await MongoMemoryServer.create()
+  connection = await mongoose.connect(mongodb.getUri(), {})
+  console.log('Connected to test database.')
+}
+
+const close = async () => {
+  await mongoose.connection.dropDatabase()
+  await mongoose.connection.close()
+  await mongodb.stop()
+  console.log('Stopped test database')
+}
+
+const clear = async () => {
+  const collections = mongoose.connection.collections
+  for (const key in collections) {
+    await collections[key].deleteMany({})
+  }
+  console.log('Cleared test database.')
+}
+module.exports = { connect, close, clear }
