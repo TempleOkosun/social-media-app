968bf2df5f6ac10c5b09976d553f5d83e9427217
Temple <okosun.temple@gmail.com>
2022-06-13 19:42:59 -0400

    adding relationship between tweet and user

--- models/tweet.js
+++ models/tweet.js
@@ -1,11 +1,16 @@
 // required imports
 const mongoose = require('mongoose')
+const { ObjectId } = mongoose.Schema
 
 const tweetSchema = new mongoose.Schema({
   body: {
     type: String,
     trim: true,
     required: true,
+  },
+  tweetedBy: {
+    type: ObjectId,
+    ref: 'User',
   },
 })
 
