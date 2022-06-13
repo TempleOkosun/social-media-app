32169a8aacce77255441852223a92d17f508cf69
Temple <okosun.temple@gmail.com>
2022-06-13 15:00:45 -0400

    debugging has authorization and user update in progress

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 // required imports
+const _ = require('lodash')
 const User = require('../models/user')
+const Tweet = require('../models/tweet')
 
 exports.userById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
@@ -16,7 +18,13 @@ exports.userById = (req, res, next, id) => {
 }
 
 exports.hasAuthorization = (req, res, next) => {
-  const authorized = req.profile && req.auth && req.profile._id === req.auth._id
+  const authorized = req.profile && req.auth && req.profile._id == req.auth
+  console.log(`Here is authorized : ${authorized}`)
+  console.log(` Here is req.profile ${req.profile}`)
+  console.log(` Here is req.auth ${req.auth}`)
+  console.log(` Here is req.profile._id ${req.profile._id}`)
+  console.log(` Here is req.profile && req.auth ${req.profile._id == req.auth}`)
+
   if (!authorized) {
     res.status(403).json({
       error: 'User is not authorized',
@@ -38,4 +46,25 @@ exports.allUsers = async (req, res) => {
 
 exports.getUser = async (req, res) => {
   return res.json(req.profile)
+}
+
+exports.updateUser = async (req, res, next) => {
+  const current_user_details = req.profile
+  console.log(current_user_details)
+  // user will be updated based on the request body
+  const updated_user_details = _.extend(current_user_details, req.body) // will mutate the source object current_user_details with req.body
+  // we can also add the user updated field at this point
+  updated_user_details.updated = Date.now()
+  console.log(updated_user_details)
+  // save updated user to database
+  const user = new User(updated_user_details)
+  user.save((err) => {
+    if (err) {
+      return res.status(400).json({
+        // error: `You are not authorized to perform this action`,
+        error: err,
+      })
+    }
+    res.json({ user })
+  })
 }
